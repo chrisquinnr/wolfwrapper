49059e53659f3dbb07f86729f24dcb0b5b513622
Chris Quinn <chris@gravitywell.co.uk>
2016-03-29 23:48:46 +0100

    oh boy this gonna be fab

--- client/controllers/authenticated/wolfram.js
+++ client/controllers/authenticated/wolfram.js
@@ -1,23 +1,32 @@
 Template.wolfram.events({
-    'click #apitest': function (e){
-        Meteor.call('callWolframAPI', function(err, resp){
-            if(err){
-                console.log(err);
-            } else {
-                if (resp) {
-                    //console.log(resp);
-                    var message = 'Wolfram API success, import ' + resp + ' complete';
-                    Bert.alert('success', message, 'growl-top-right');
-                }
-            }
-        });
+  'click .apitest': function ( e ) {
+
+    var input = $('.inputtext').val();
+    if(!input) {
+      Bert.alert('Omg no input', 'danger', 'growl-top-right');
+      return false;
+    } else {
+      console.log(input);
     }
+
+    Meteor.call('callWolframAPI', input, function ( err, resp ) {
+      if (err) {
+        console.log(err);
+      } else {
+        if (resp) {
+          //console.log(resp);
+          var message = 'Wolfram API success, import ' + resp + ' complete';
+          Bert.alert('success', message, 'growl-top-right');
+        }
+      }
+    });
+  }
 })
 
 Template.wolfram.helpers({
-    getWolframOutput: function (){
-        //console.log('getting saved response');
-        var data = Queries.find({});
-        return data;
-    }
+  getWolframOutput: function () {
+    //console.log('getting saved response');
+    var data = Queries.find({});
+    return data;
+  }
 })
--- server/methods/utility/wolframalpha.js
+++ server/methods/utility/wolframalpha.js
@@ -1,50 +1,65 @@
 var Future = Npm.require('fibers/future');
 
 Meteor.methods({
-    'callWolframAPI': function(){
-        console.log('invoking wrapper... (1)');
-        Meteor.call('wolframAPIWrapper', function(err, resp){
-            if(err){
-                console.log('Error in Wolfram API processing, response not captured');
-            } else {
-                if(resp){
-                    console.log('......complete (1)');
-                    var result = Meteor.call('parseResponse', resp);
-                    var meta = result.queryresult.$;
-                    var pods = result.queryresult.pod;
-                    console.log(pods[0].subpod.plaintext);
-                    console.log(pods[0].subpod.img);
-                    console.log(pods[1].subpod.plaintext);
-                    //console.log(pods[2]);
-                    //console.log(pods[3]);
-                    //console.log(pods[4]);
-                    //console.log(pods[5]);
-                    //console.log(pods[6]);
-                    //console.log(pods[7]);
-                    //console.log(pods[8]);
-                    //console.log(pods[9]);
-                    return;
-                    return Queries.insert({related: meta.related, timing: meta.timing, pods: pods});
-                }
-            }
-        });
+  'callWolframAPI': function (input) {
+    console.log('We made it onto the server, with input: ' + input);
+    if(!input) return false;
+    input = input.toLowerCase();
+    console.log('invoking wrapper... (1)');
+    Meteor.call('wolframAPIWrapper', input, function ( err, resp ) {
+      if (err) {
+        console.log('Error in Wolfram API processing, response not captured');
+      } else {
+        if (resp) {
+          console.log('......complete (1)');
+          var result = Meteor.call('parseResponse', resp);
 
-    },
-    'wolframAPIWrapper': function(){
-        console.log('passing on request... (2)');
-        var promise = new Future();
-        Meteor.call('wolframAPI', function(err, resp){
-            if(resp){
-                console.log('......complete (2)');
-                promise.return(resp);
+          // check meta
+          var meta = _.first(_.values(_.pick(result.queryresult, '$')));
+          if(meta.success !== 'true'){
+            console.log(meta);
+            return false;
+          } else {
+            console.log('meta says this is good')
+          }
+
+          //loop pods
+          var pods = result.queryresult.pod;
+          console.log(pods);
+          _.each(pods, function(elem){
+            var podelem = _.first(_.values(_.pick(elem, '$')));
+            if(podelem.title === 'Organism weight'){
+              var subpod = _.pick(elem, 'subpod');
+              console.log(_.pick(subpod.subpod[0], '$'));
+              console.log(_.pick(subpod.subpod[0], 'img'));
+              console.log(_.pick(subpod.subpod[0], 'plaintext'));
+              var state = _.pick(elem, 'states');
+              console.log(state);
             }
-        });
-        return promise.wait();
-    },
-    'wolframAPI': function(){
-        //check(params, Object);
-        var endpoint = "http://api.wolframalpha.com/v2/query?input=pi&appid=6TKGHR-L4T3W4R5RY";
-        console.log('sending POST request');
-        return HTTP.call("POST", endpoint);
-    }
+          });
+
+          return true;
+          //Queries.insert({related: meta.related, timing: meta.timing, pods: pods});
+        }
+      }
+    });
+
+  },
+  'wolframAPIWrapper': function (input) {
+    console.log('passing on request... (2)');
+    var promise = new Future();
+    Meteor.call('wolframAPI', input, function ( err, resp ) {
+      if (resp) {
+        console.log('......complete (2)');
+        promise.return(resp);
+      }
+    });
+    return promise.wait();
+  },
+  'wolframAPI': function (input) {
+    //check(params, Object);
+    var endpoint = "http://api.wolframalpha.com/v2/query?input="+input+"&appid=6TKGHR-L4T3W4R5RY";
+    console.log('sending POST request');
+    return HTTP.call("POST", endpoint);
+  }
 });
